664f762ea0bcf701ddce085991791d780e8278d3
Patrick Maloney <pmalone9@jhu.edu>
2017-01-16 17:05:47 -0500

    I'm trying to get collisions to work so the player is bounded by the screen

--- Top Down Untitled/GameScene.swift
+++ Top Down Untitled/GameScene.swift
@@ -9,16 +9,22 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var gameOver = false
+
+    // this will change based on where the player moves to, different rooms will have slightly different layouts
+    var roomType = "basic"
+    
     
     // player
     let player = SKSpriteNode(imageNamed: "playerImage.png")
     
     
     // joystick that changes the players rotation
-    let turnStick = SKSpriteNode(imageNamed: "dot.png")     // joystick
-    let tsBase = SKSpriteNode(imageNamed: "dot.png")        // j2
-    var tsActive = false                                    // stickActive
+    let turnStick = SKSpriteNode(imageNamed: "dot.png")
+    let tsBase = SKSpriteNode(imageNamed: "dot.png")
+    var tsActive = false
     
     
     // joystick that moves the player
@@ -27,21 +33,43 @@ class GameScene: SKScene {
     var msActive = false
     
     
+    // setting up collisions
+    enum colliderType: UInt32 {
+        case Player = 1
+        case Environment = 2
+        //case Enemy = 4        // don't need this yet
+    }
+    
+    
+    
+    
+    
     
     override func didMove(to view: SKView) {
-        self.backgroundColor = SKColor.white
-        self.anchorPoint = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        //self.physicsWorld.contactDelegate = self
+        setupGame()
+    }
+    
+    
+    
+    
+    
+    func setupGame() {
+        
+        // this will set up the rooms
+        roomSetup(roomType: roomType)
+        
         
         
         // rotation stick
         self.addChild(tsBase)
         tsBase.position = CGPoint(x: self.frame.midX + (self.frame.width / 3), y: self.frame.midY)
-        // should probably make these a percentage of the screen size
         tsBase.scale(to: CGSize(width: 150, height: 150))
         
         self.addChild(turnStick)
         turnStick.position = CGPoint(x: self.frame.midX + (self.frame.width / 3), y: self.frame.midY)
         turnStick.scale(to: CGSize(width: 100, height: 100))
+        
         
         
         // move stick
@@ -54,18 +82,87 @@ class GameScene: SKScene {
         moveStick.scale(to: CGSize(width: 100, height: 100))
         
         
+        
         // player
-        self.addChild(player)
-        player.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        player.position = CGPoint(x: self.frame.midX, y: 2 * self.frame.midX)
+        //player.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
         player.scale(to: CGSize(width: 75, height: 75))
+        player.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: player.size.width, height: player.size.height))
+        player.physicsBody?.isDynamic = false
+        
+        player.physicsBody?.contactTestBitMask = colliderType.Environment.rawValue
+        player.physicsBody?.categoryBitMask = colliderType.Player.rawValue
+        player.physicsBody?.collisionBitMask = colliderType.Player.rawValue
+        self.addChild(player)
+        
         
         // changes the opacity
         turnStick.alpha = 0.4
         tsBase.alpha = 0.4
         moveStick.alpha = 0.4
         msBase.alpha = 0.4
-        
     }
+    
+    
+    func roomSetup(roomType: String) {
+        
+        
+        
+        
+        let ltWall = SKSpriteNode(imageNamed: "wall")
+        let rtWall = SKSpriteNode(imageNamed: "wall")
+        ltWall.scale(to: CGSize(width: self.frame.width / 10, height: self.frame.height))
+        rtWall.scale(to: CGSize(width: self.frame.width / 10, height: self.frame.height))
+        
+        ltWall.position = CGPoint(x: 0, y: self.frame.height / 2)
+        ltWall.physicsBody = SKPhysicsBody(rectangleOf: ltWall.size)
+        rtWall.position = CGPoint(x: self.frame.width, y: self.frame.height / 2)
+        rtWall.physicsBody = SKPhysicsBody(rectangleOf: rtWall.size)
+        
+        
+        
+        
+        let btWall = SKSpriteNode(imageNamed: "wall")
+        let tpWall = SKSpriteNode(imageNamed: "wall")
+        btWall.scale(to: CGSize(width: self.frame.width, height: self.frame.height / 10))
+        tpWall.scale(to: CGSize(width: self.frame.width, height: self.frame.height / 10))
+        
+        btWall.position = CGPoint(x: self.frame.height / 2, y: self.frame.width - self.frame.width / 3)     // fix position
+        btWall.physicsBody = SKPhysicsBody(rectangleOf: btWall.size)
+        tpWall.position = CGPoint(x: self.frame.width / 2, y: 0)            // fix position
+        tpWall.physicsBody = SKPhysicsBody(rectangleOf: tpWall.size)
+        
+        print("midX = \(self.frame.midX)\nmidY = \(self.frame.midY)\nheight = \(self.frame.height)\nwidth = \(self.frame.width)")
+        
+        let wallArray = [ltWall, rtWall, btWall, tpWall]
+        
+        for w in wallArray {
+            
+            w.physicsBody!.isDynamic = false
+            
+            w.physicsBody?.contactTestBitMask = colliderType.Environment.rawValue
+            w.physicsBody?.categoryBitMask = colliderType.Environment.rawValue
+            w.physicsBody?.collisionBitMask = colliderType.Environment.rawValue
+            
+            
+            self.addChild(w)
+            print("wall added")
+            
+        }
+        
+        
+        switch roomType {
+        case "basic":
+            
+            self.backgroundColor = SKColor.white
+            self.anchorPoint = CGPoint(x: self.frame.midX, y: self.frame.midY)
+            
+            
+        default:
+            break
+        }
+    }
+    
     
     
     
@@ -90,12 +187,17 @@ class GameScene: SKScene {
     
     
     
+    
+    // there is a problem with the movement, if the user holds their finger still while on the joystick the player will not move
+    // same problem with the rotation, if we want the player to automatically shoot if the player's thumb is on the turnstick, it won't shoot if they hold their thumb still
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches {
             let location = t.location(in: self)
             
             // determining if the turning stick is being used
             if tsActive {
+                
+                
                 let tsVector = CGVector(dx: location.x - tsBase.position.x, dy: location.y - tsBase.position.y)
                 let tsAngle = atan2(tsVector.dy, tsVector.dx)         // will be in radians
                 let tsLength:CGFloat = tsBase.frame.size.height / 2
@@ -106,11 +208,14 @@ class GameScene: SKScene {
                 } else {
                     turnStick.position = CGPoint(x: tsBase.position.x - tsXDist, y: tsBase.position.y + tsYDist)
                 }
-                player.zRotation = tsAngle
+                player.zRotation = tsAngle      //to fix the issue stated above, maybe have this line be in setupGame or something
             }   // ends tsActive test
+            
             
             // determining if the moving stick is being used
             if msActive {
+                
+                
                 let msVector = CGVector(dx: location.x - msBase.position.x, dy: location.y - msBase.position.y)
                 let msAngle = atan2(msVector.dy, msVector.dx)         // will be in radians
                 let msLength:CGFloat = msBase.frame.size.height / 2
@@ -121,7 +226,10 @@ class GameScene: SKScene {
                 } else {
                     moveStick.position = CGPoint(x: msBase.position.x - msXDist, y: tsBase.position.y + msYDist)
                 }
-                player.position = CGPoint(x: player.position.x + (msVector.dx / 10), y: player.position.y + (msVector.dy / 10))
+                player.position = CGPoint(x: player.position.x + (msVector.dx / 20), y: player.position.y + (msVector.dy / 20))     //to fix the issue stated above, maybe have this line be in setupGame or something
+                // adjust the movement speed as needed
+                
+                
             }   // ends tsActive test
             
         }   // ends for loop
@@ -134,18 +242,34 @@ class GameScene: SKScene {
         
             // turning
             if tsActive {
-                let move = SKAction.move(to: tsBase.position, duration: 0.1)
-                move.timingMode = .easeOut
-                turnStick.run(move)
+                let centerStick = SKAction.move(to: tsBase.position, duration: 0.1)
+                centerStick.timingMode = .easeOut
+                turnStick.run(centerStick)
             }
             
             // moving
             if msActive {
-                let move = SKAction.move(to: msBase.position, duration: 0.1)
-                move.timingMode = .easeOut
-                moveStick.run(move)
+                let centerStick = SKAction.move(to: msBase.position, duration: 0.1)
+                centerStick.timingMode = .easeOut
+                moveStick.run(centerStick)
             }
 
+    }
+    
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if !gameOver {
+            
+            // this checks if the player hits the wall
+            if contact.bodyA.categoryBitMask == colliderType.Environment.rawValue || contact.bodyB.categoryBitMask == colliderType.Environment.rawValue {
+                print("Hit a wall")
+                
+            } else {
+                print("contact")
+            }
+            
+            
+        }   // end game over check
     }
     
     
