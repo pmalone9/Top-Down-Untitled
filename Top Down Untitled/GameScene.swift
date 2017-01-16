5bee984d752d480a6955e32cafeb2de0f1db541a
Patrick Maloney <pmalone9@jhu.edu>
2017-01-16 00:13:05 -0500

    Added joystick functionality Left stick is to move player, right stick is to rotate them Next step is to add borders around the edge of the screen so the player is contained to the screen

--- Top Down Untitled/GameScene.swift
+++ Top Down Untitled/GameScene.swift
@@ -11,76 +11,144 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    // player
+    let player = SKSpriteNode(imageNamed: "playerImage.png")
+    
+    
+    // joystick that changes the players rotation
+    let turnStick = SKSpriteNode(imageNamed: "dot.png")     // joystick
+    let tsBase = SKSpriteNode(imageNamed: "dot.png")        // j2
+    var tsActive = false                                    // stickActive
+    
+    
+    // joystick that moves the player
+    let moveStick = SKSpriteNode(imageNamed: "dot.png")
+    let msBase = SKSpriteNode(imageNamed: "dot.png")
+    var msActive = false
+    
+    
     
     override func didMove(to view: SKView) {
+        self.backgroundColor = SKColor.white
+        self.anchorPoint = CGPoint(x: self.frame.midX, y: self.frame.midY)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        // rotation stick
+        self.addChild(tsBase)
+        tsBase.position = CGPoint(x: self.frame.midX + (self.frame.width / 3), y: self.frame.midY)
+        // should probably make these a percentage of the screen size
+        tsBase.scale(to: CGSize(width: 150, height: 150))
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        self.addChild(turnStick)
+        turnStick.position = CGPoint(x: self.frame.midX + (self.frame.width / 3), y: self.frame.midY)
+        turnStick.scale(to: CGSize(width: 100, height: 100))
+        
+        
+        // move stick
+        self.addChild(msBase)
+        msBase.position = CGPoint(x: self.frame.midX - (self.frame.width / 3), y: self.frame.midY)
+        msBase.scale(to: CGSize(width: 150, height: 150))
+        
+        self.addChild(moveStick)
+        moveStick.position = CGPoint(x: self.frame.midX - (self.frame.width / 3), y: self.frame.midY)
+        moveStick.scale(to: CGSize(width: 100, height: 100))
+        
+        
+        // player
+        self.addChild(player)
+        player.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        player.scale(to: CGSize(width: 75, height: 75))
+        
+        // changes the opacity
+        turnStick.alpha = 0.4
+        tsBase.alpha = 0.4
+        moveStick.alpha = 0.4
+        msBase.alpha = 0.4
+        
     }
     
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for t in touches {
+            let location = t.location(in: self)
+            
+            if(tsBase.frame.contains(location)) {
+                tsActive = true
+            } else {
+                tsActive = false
+            }
+            
+            if(msBase.frame.contains(location)) {
+                msActive = true
+            } else {
+                msActive = false
+            }
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
+    
+    
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for t in touches {
+            let location = t.location(in: self)
+            
+            // determining if the turning stick is being used
+            if tsActive {
+                let tsVector = CGVector(dx: location.x - tsBase.position.x, dy: location.y - tsBase.position.y)
+                let tsAngle = atan2(tsVector.dy, tsVector.dx)         // will be in radians
+                let tsLength:CGFloat = tsBase.frame.size.height / 2
+                let tsXDist:CGFloat = sin(tsAngle - 1.57079633) * tsLength
+                let tsYDist:CGFloat = cos(tsAngle - 1.57079633) * tsLength
+                if(tsBase.frame.contains(location)) {
+                    turnStick.position = location
+                } else {
+                    turnStick.position = CGPoint(x: tsBase.position.x - tsXDist, y: tsBase.position.y + tsYDist)
+                }
+                player.zRotation = tsAngle
+            }   // ends tsActive test
+            
+            // determining if the moving stick is being used
+            if msActive {
+                let msVector = CGVector(dx: location.x - msBase.position.x, dy: location.y - msBase.position.y)
+                let msAngle = atan2(msVector.dy, msVector.dx)         // will be in radians
+                let msLength:CGFloat = msBase.frame.size.height / 2
+                let msXDist:CGFloat = sin(msAngle - 1.57079633) * msLength
+                let msYDist:CGFloat = cos(msAngle - 1.57079633) * msLength
+                if(msBase.frame.contains(location)) {
+                    moveStick.position = location
+                } else {
+                    moveStick.position = CGPoint(x: msBase.position.x - msXDist, y: tsBase.position.y + msYDist)
+                }
+                player.position = CGPoint(x: player.position.x + (msVector.dx / 10), y: player.position.y + (msVector.dy / 10))
+            }   // ends tsActive test
+            
+        }   // ends for loop
     }
+    
+    
+    
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
+            // turning
+            if tsActive {
+                let move = SKAction.move(to: tsBase.position, duration: 0.1)
+                move.timingMode = .easeOut
+                turnStick.run(move)
+            }
+            
+            // moving
+            if msActive {
+                let move = SKAction.move(to: msBase.position, duration: 0.1)
+                move.timingMode = .easeOut
+                moveStick.run(move)
+            }
+
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
+    
     
     
     override func update(_ currentTime: TimeInterval) {
